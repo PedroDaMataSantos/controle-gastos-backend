3020e5517b41226cd63fedbe3c85302dd3462d25
pedro.santos <pescariaurbana@gmail.com>
2026-04-27 15:13:21 -0300

    Rewstrições adicionadas

--- src/main/java/com/damatapedro/controle_gastos/enums/Categorias.java
+++ src/main/java/com/damatapedro/controle_gastos/enums/Categorias.java
@@ -2,32 +2,38 @@ package com.damatapedro.controle_gastos.enums;
 
 public enum Categorias {
 
-    //Gastos
-    ALIMENTACAO("Alimentação"),
-    LAZER("Lazer"),
-    COMBUSTIVEL("Combustível"),
-    SAUDE ("Saúde"),
-    EDUCACAO ("Educação"),
-    FATURA_CARTAO("Fatura do cartão"),
-    ALUGUEL("Aluguel"),
-    CONTAS("Contas"),
-    OUTROS("Outras despesas"),
-    TRIBUTO("Impostos"),
+    // GASTOS
+    ALIMENTACAO("Alimentação", TipoRegistro.GASTO),
+    LAZER("Lazer", TipoRegistro.GASTO),
+    COMBUSTIVEL("Combustível", TipoRegistro.GASTO),
+    SAUDE("Saúde", TipoRegistro.GASTO),
+    EDUCACAO("Educação", TipoRegistro.GASTO),
+    FATURA_CARTAO("Fatura do cartão", TipoRegistro.GASTO),
+    ALUGUEL("Aluguel", TipoRegistro.GASTO),
+    CONTAS("Contas", TipoRegistro.GASTO),
+    OUTROS("Outros gastos", TipoRegistro.GASTO),
+    TRIBUTO("Impostos", TipoRegistro.GASTO),
 
-    //Receita
-    SALARIO("Salario"),
-    INVESTIMENTO("Investimento"),
-    VALES("Vales"),
-    BENEFICIO("Benefícios");
+    // RECEITAS
+    SALARIO("Salário", TipoRegistro.RECEITA),
+    INVESTIMENTO("Investimento", TipoRegistro.RECEITA),
+    VALES("Vales", TipoRegistro.RECEITA),
+    BENEFICIO("Benefícios", TipoRegistro.RECEITA),
+    OUTRAS("Outras receitas", TipoRegistro.RECEITA);
 
     private final String descricao;
+    private final TipoRegistro tipo;
 
-    private Categorias(String descricao) {
+    Categorias(String descricao, TipoRegistro tipo) {
         this.descricao = descricao;
+        this.tipo = tipo;
     }
 
     public String getDescricao() {
         return descricao;
     }
-}
 
+    public TipoRegistro getTipo() {
+        return tipo;
+    }
+}
