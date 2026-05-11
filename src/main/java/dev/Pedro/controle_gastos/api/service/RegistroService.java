7fdab4ef560a65e352f8d083dd52abe46517a2da
PedroDaMataSantos <pescariaurbana@gmail.com>
2026-05-11 19:19:37 -0300

    Crud realizado

--- src/main/java/dev/Pedro/controle_gastos/api/service/RegistroService.java
+++ src/main/java/dev/Pedro/controle_gastos/api/service/RegistroService.java
@@ -2,10 +2,13 @@ package dev.Pedro.controle_gastos.api.service;
 
 import dev.Pedro.controle_gastos.domain.model.Registro;
 import dev.Pedro.controle_gastos.domain.repository.RegistroRepository;
+import dev.Pedro.controle_gastos.enums.Categorias;
+import dev.Pedro.controle_gastos.enums.TipoRegistro;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 @Transactional
@@ -65,7 +68,66 @@ public class RegistroService {
         return repository.save(registro);
     }
 
+    public Registro update(Long id, Registro novoRegistro){
 
+        //Valida se o registro existe e retorna o erro
+        Registro registroExistente = repository.findById(id).orElseThrow(()->new RuntimeException("Registro não encontrado"));
+
+        //Update
+        registroExistente.setTipoRegistro(novoRegistro.getTipoRegistro());
+
+        registroExistente.setCategoria(novoRegistro.getCategoria());
+
+        registroExistente.setDescricao(novoRegistro.getDescricao());
+
+        registroExistente.setValor(novoRegistro.getValor());
+
+        registroExistente.setData(novoRegistro.getData());
+
+        //Valida denovo , para não ocorrer erros
+        validaCamposObg(registroExistente);
+        validarCategoria_Tipo(registroExistente);
+
+        //Salva as novas entradas
+        return repository.save(registroExistente);
+
+    }
+
+    public void delete(Long id) {
+
+        //Verifica se existe
+        Registro registro = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Registro não existe"));
+
+        //Exclui
+        repository.delete(registro);
+    }
+
+
+        //Readers
+
+    public Registro buscaPorId(Long id){
+
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Registro não encontrado"));
+
+    }
+
+    public List<Registro> buscaPorCategoria(Categorias categoria){
+
+        return repository.findByCategoria(categoria);
+
+    }
+
+    public List<Registro> buscarPorPeriodo(LocalDate inicio, LocalDate fim) {
+
+        return repository.findByDataBetween(inicio, fim);
+
+    }
+
+    public List<Registro> buscarPorTipo(TipoRegistro tipo) {
+
+        return repository.findByTipoRegistro(tipo);
+    }
 
 
 }
