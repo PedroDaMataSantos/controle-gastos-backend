e521131eb7052862d40f570db31df95505a4471f
Gabriel Souza de Oliveira <gabriel.solid@Planaltos-MacBook-Air.local>
2026-06-10 12:46:09 -0300

    Controller
    Foi concluído o CRUD no controller

--- src/main/java/dev/Pedro/controle_gastos/api/controller/RegistroController.java
+++ src/main/java/dev/Pedro/controle_gastos/api/controller/RegistroController.java
@@ -4,9 +4,12 @@ package dev.Pedro.controle_gastos.api.controller;
 import dev.Pedro.controle_gastos.api.dto.RegistroRequest;
 import dev.Pedro.controle_gastos.api.dto.RegistroResponse;
 import dev.Pedro.controle_gastos.api.service.RegistroService;
+import dev.Pedro.controle_gastos.enums.Categorias;
+import dev.Pedro.controle_gastos.enums.TipoRegistro;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -27,6 +30,45 @@ public class RegistroController {
         return service.create(registroRequest);
     }
 
+    @GetMapping("/{id}")
+    public RegistroResponse getId(@PathVariable Long id){
+        return service.buscaPorId(id);
+
+    }
+
+    @GetMapping("/categoria/{categoria}")
+     public List <RegistroResponse> getCategoria(@PathVariable Categorias categoria){
+        return service.buscaPorCategoria(categoria);
+
+    }
+
+    @GetMapping("/tipo/{tipo}")
+    public List <RegistroResponse> getTipo(@PathVariable TipoRegistro tipo){
+        return service.buscarPorTipo(tipo);
+
+    }
+
+    @GetMapping("/periodo")
+    public List<RegistroResponse> getPeriodo(
+            @RequestParam LocalDate inicio,
+            @RequestParam LocalDate fim) {
+
+        return service.buscarPorPeriodo(inicio, fim);
+    }
+
+    @PutMapping("/{id}")
+    public RegistroResponse update(@PathVariable Long id ,@RequestBody RegistroRequest req){
+        return service.update(id,req);
+
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+
+    public void  delete(@PathVariable Long id){
+         service.delete(id);
+
+    }
 
 
 
